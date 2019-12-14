b55ce461d6d320fcf54fb97d258befe2be4b20fb
bryanhoneck <bryan.honeck@gmail.com>
2019-12-14 15:34:13 -0600

    This second commit demonstrates some real progress in the program. It really shows the direction it is headed in! Some comments were added, but more importantly, I imported and used the java swing package for the users to answer the questions.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 
 public class Main
 {
@@ -6,5 +7,13 @@ public class Main
             String welcome = "I hope you're ready to play some Mad Libs!";
             String response[] = {"", "", "", "", ""};
 
-        }
-}
+            //The beginning of the questioning - we used JOptionPane in Prof Benavides' class at Collin!
+            JOptionPane.showMessageDialog(null, welcome);
+            response[0] = JOptionPane.showInputDialog("Answer my questions and I will give you the " +
+                    "best day of your life!\nWhat's your favorite type of music?", "Enter response here");
+            response[1] = JOptionPane.showInputDialog("What's your favorite sport?", "Enter response here");
+            response[2] = JOptionPane.showInputDialog("What's your favorite dessert?", "Enter response here");
+            response[3] = JOptionPane.showInputDialog("What is your favorite color?", "Enter response here");
+            response[4] = JOptionPane.showInputDialog("What state were you born in?", "Enter response here");
+        }//End main method
+}//End class Main
