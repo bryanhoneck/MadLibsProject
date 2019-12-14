2420a0a88212b0da170e11c88bd7a92e25ca5011
bryanhoneck <bryan.honeck@gmail.com>
2019-12-14 15:48:58 -0600

    This third and final commit finishes this project! I made up my own little story to go along with all of the questions I asked. I played it with my dad, and he thought it was fun!

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,11 @@ public class Main
             response[2] = JOptionPane.showInputDialog("What's your favorite dessert?", "Enter response here");
             response[3] = JOptionPane.showInputDialog("What is your favorite color?", "Enter response here");
             response[4] = JOptionPane.showInputDialog("What state were you born in?", "Enter response here");
+
+            JOptionPane.showMessageDialog(null, "The best day of your life will take place on a nice " +
+                    "sunny day \nin " + response[4] + ", where you will be watching the best " + response[1] + " game \n" +
+                    "on TV. You will be sitting in your comfy " + response[3] + " chair with a lifetime\n supply of " +
+                    response[2] + " . After the game, you'll ask people what kind of music \nthey want to listen to " +
+                    "while they finish their food, and they'll all shout " + response[0] + "!");
         }//End main method
 }//End class Main
