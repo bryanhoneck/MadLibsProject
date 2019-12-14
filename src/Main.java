091fb24596cf3edf1594733863ab08bcae6467e2
bryanhoneck <bryan.honeck@gmail.com>
2019-12-14 15:22:41 -0600

    This is just the beginning of the madlib project! I've set up a couple strings that will help me develop this program with ease!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main
+{
+    public static void main (String[] args)
+        {
+            String welcome = "I hope you're ready to play some Mad Libs!";
+            String response[] = {"", "", "", "", ""};
+
+        }
+}
